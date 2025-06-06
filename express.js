a32c7e130d04d07494939e25caba380db5f67d38
Anushka <899anushkasingh@gmail.com>
2025-06-06 23:48:42 +0530

    updated port for render

--- express.js
+++ express.js
@@ -120,7 +120,9 @@ app.get('/logs', (req, res) => {
   });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Job Scheduler running on http://localhost:${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Job Scheduler running on http://localhost:${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3000;
